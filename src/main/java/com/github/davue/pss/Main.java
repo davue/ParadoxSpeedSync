657442f48828d457099c05909c637deea1812a5e
davue <davue@bluewin.ch>
2020-02-09 16:29:02 +0100

    WIP Sync changes to other device

--- src/main/java/com/github/davue/pss/Main.java
+++ src/main/java/com/github/davue/pss/Main.java
@@ -18,19 +18,43 @@
 
 package com.github.davue.pss;
 
-import com.github.davue.pss.client.Client;
-import com.github.davue.pss.server.Server;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        if (args.length >= 1 && args[0].toLowerCase().equals("-s")) {
-            new Server(Integer.parseInt(args[1])).run();
-        } else if (args.length >= 2 && args[0].toLowerCase().equals("-c")) {
-            new Client(args[1], Integer.parseInt(args[2])).run();
-        } else {
-            System.out.println("Usage:\n" +
-                    "    Server: -s <port>\n" +
-                    "    Client: -c <host> <port>");
-        }
+//        if (args.length >= 1 && args[0].toLowerCase().equals("-s")) {
+//            new Server(Integer.parseInt(args[1])).start();
+//        } else if (args.length >= 2 && args[0].toLowerCase().equals("-c")) {
+//            new Client(args[1], Integer.parseInt(args[2])).start();
+//        } else {
+//            System.out.println("Usage:\n" +
+//                    "    Server: -s <port>\n" +
+//                    "    Client: -c <host> <port>");
+//        }
+
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Text testText1 = new Text("Test");
+        Text testText2 = new Text("Soos");
+
+        GridPane root = new GridPane();
+        root.setGridLinesVisible(true);
+        root.add(testText1, 0, 0);
+        root.add(testText2, 0, 1);
+
+        Scene scene = new Scene(root);
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
--- src/main/java/com/github/davue/pss/client/Client.java
+++ src/main/java/com/github/davue/pss/client/Client.java
@@ -24,7 +24,6 @@ import org.jnativehook.NativeHookException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 
